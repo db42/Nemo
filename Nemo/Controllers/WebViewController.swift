05f58158b8b3ef7263a263ac853f9ff2b0dd0f12
Dushyant Bansal <dushyant.bansal@thoughtspot.com>
2018-10-07 13:55:22 +0530

    Get rid of search bar and search controller

--- Nemo/Controllers/WebViewController.swift
+++ Nemo/Controllers/WebViewController.swift
@@ -11,21 +11,19 @@ import UIKit
 class NemoWebView: UIWebView {
 }
 
-protocol SearchResultsDelegate: class {
-  func didSelectURL(_ url: URL)
-}
-
-class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognizerDelegate, SearchResultsDelegate, UISearchControllerDelegate, UISearchBarDelegate, UIScrollViewDelegate {
+class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognizerDelegate, UIScrollViewDelegate {
 
   var currentOffset: CGFloat = 0.0
   @IBOutlet weak var searchView: UIView!
-
+  @IBOutlet weak var searchTextField: UITextField!
+  
+  @IBOutlet weak var searchResultsTableView: UITableView!
   @IBOutlet weak var webView: NemoWebView!
   weak var delegate: MainVCWebDelegate?
   
   @IBOutlet weak var searchViewHeightConstraint: NSLayoutConstraint!
-  var searchController: UISearchController!
   var url: URL?
+  var searchResults: [URL] = []
   
   func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
     return true;
@@ -46,42 +44,23 @@ class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognize
     webView.scrollView.delegate = self
     webView.scrollView.addObserver(self, forKeyPath: "contentOffset", options: .new, context: nil)
 
+    searchResultsTableView.dataSource = self
+    searchResultsTableView.delegate = self
     
     let hideKeyboardGesture = UIGestureRecognizer()
     hideKeyboardGesture.delegate = self
     webView.addGestureRecognizer(hideKeyboardGesture)
-    
-    let sb = UIStoryboard(name: "Main", bundle: nil)
-    let srController = sb.instantiateViewController(withIdentifier: "URLSearchResultsController") as! URLSearchResultsController
-    srController.delegate = self
-    searchController = UISearchController(searchResultsController: srController)
-    searchController.searchBar.frame = searchView.bounds
-    searchController.searchResultsUpdater = srController
-    searchController.delegate = self
-    searchController.searchBar.delegate = self
-    
-    searchView.addSubview(searchController.searchBar)
-    searchController.searchBar.autoresizingMask = UIView.AutoresizingMask.flexibleWidth
-    
-    searchController.searchBar.returnKeyType = UIReturnKeyType.go
-    searchController.searchBar.autocapitalizationType = UITextAutocapitalizationType.none
-    searchView.bringSubviewToFront(searchController.searchBar)
   }
   
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-    searchController.searchBar.subviews[0].subviews[0].frame = searchView.bounds
-  }
-  
-  //MARK: SearchControllerDelegate
-  func didDismissSearchController(_ searchController: UISearchController) {
-    searchController.searchBar.text = webView.request?.url?.absoluteString
+  fileprivate func hideSearchResultsTableView() {
+    searchResultsTableView.isHidden = true
   }
   
   // MARK: SearchResultsDelegate
   func didSelectURL(_ url: URL) {
-    searchController.searchBar.text = url.absoluteString
-    self.dismiss(animated: true, completion: nil)
+    searchTextField.text = url.absoluteString
+    searchTextField.resignFirstResponder()
+    hideSearchResultsTableView()
     webView.loadRequest(URLRequest(url: url))
   }
   
@@ -93,20 +72,31 @@ class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognize
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     
-    if let text = searchController.searchBar.text, text != "" {
+    if let text = searchTextField.text, text != "" {
       return
     }
     
     if let url = self.url {
       webView.loadRequest(URLRequest(url: url))
-      searchController.searchBar.text = url.absoluteString
+      searchTextField.text = url.absoluteString
     } else {
-      searchController.searchBar.becomeFirstResponder()
+      searchTextField.becomeFirstResponder()
     }
   }
   
   func reloadPage() {
     webView.reload()
+  }
+  
+  fileprivate func openWebPageForUserText(_ text: String) {
+    let str = text.hasPrefix("http") ? text : "http://\(text)"
+    
+    if let url = URL(string: str), url.absoluteString.range(of: ".") != nil {
+      webView.loadRequest(URLRequest(url: url))
+    } else {
+      let txt = "https://www.google.com/search?q=\(text)".addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+      webView.loadRequest(URLRequest(url: URL(string: txt!)!))
+    }
   }
   
   // MARK: - UIWebViewDelegate
@@ -126,7 +116,7 @@ class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognize
     
     if let url = webView.request?.url {
       WebHistory.defaultHistory.addURL(url)
-      searchController.searchBar.text = url.absoluteString
+      searchTextField.text = url.absoluteString
     }
     // JS Injection hack to solve the target="_blank" issue and open a real browser in such case.
     let JSInjection = "javascript: var allLinks = document.getElementsByTagName('a'); if (allLinks) {var i;for (i=0; i<allLinks.length; i++) {var link = allLinks[i];var target = link.getAttribute('target'); if (target && target == '_blank') {link.setAttribute('target','_self');link.href = 'newtab:'+link.href;}}}"
@@ -149,6 +139,7 @@ class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognize
   func webViewDidStartLoad(_ webView: UIWebView) {
     UIApplication.shared.isNetworkActivityIndicatorVisible = true
   }
+
   
   // MARK: search bar delegate
   func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
@@ -159,6 +150,7 @@ class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognize
       textField.selectAll(nil)
     }
   }
+
   
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
     dismiss(animated: true, completion: nil)
@@ -166,14 +158,7 @@ class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognize
       return
     }
     
-    let str = text.hasPrefix("http") ? text : "http://\(text)"
-    
-    if let url = URL(string: str), url.absoluteString.range(of: ".") != nil {
-      webView.loadRequest(URLRequest(url: url))
-    } else {
-      let txt = "https://www.google.com/search?q=\(text)".addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
-      webView.loadRequest(URLRequest(url: URL(string: txt!)!))
-    }
+    openWebPageForUserText(text)
   }
   
   //MARK: - UIScrollViewDelegate
@@ -195,4 +180,71 @@ class WebViewController: UIViewController, UIWebViewDelegate, UIGestureRecognize
     }
   }
   
+  @IBAction func handleTextFieldEdit(_ sender: UITextField) {
+    guard let text = sender.text else {
+      return
+    }
+    //show table view
+    searchResultsTableView.isHidden = false
+    
+    //update and reload table view data
+    updateSearchResults(for: text)
+    searchResultsTableView.reloadData()
+  }
+}
+
+extension WebViewController: UITextFieldDelegate {
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    textField.selectAll(nil)
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    if let text = textField.text {
+      openWebPageForUserText(text)
+    }
+    
+    hideSearchResultsTableView()
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
+}
+
+extension WebViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  func updateSearchResults(for text: String) {
+    searchResults = WebHistory.defaultHistory.searchForText(text)
+  }
+  
+  // MARK: - Table view data source
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return searchResults.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier")
+    if (cell == nil) {
+      cell = UITableViewCell(style: .default, reuseIdentifier: "reuseIdentifier")
+    }
+    let url = searchResults[indexPath.row]
+    cell!.textLabel?.text = url.host! + url.path
+    return cell!
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard indexPath.row < searchResults.count else {
+      return
+    }
+    
+    let url = searchResults[indexPath.row]
+    didSelectURL(url)
+  }
 }
